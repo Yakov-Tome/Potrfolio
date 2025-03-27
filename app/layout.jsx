32c14ec521d825d754ce7bb94941bf3efc764966
Yakov Tome <167262858+Yakov-Tome@users.noreply.github.com>
2025-03-27 11:35:51 +0200

    Remove Analystics

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,6 @@ import Navbar from "./components/Navbar.jsx";
 import { ContentProvider } from "@/Context/GlobalContext.jsx";
 import Footer from "./components/Footer";
 import CustomCursor from "./components/CustomCursor";
-import { Analytics } from "@vercel/analytics/next";
 
 // Load Google Font
 const poppins = Poppins({
@@ -34,7 +33,6 @@ export default function RootLayout({ children }) {
           <Footer />
         </ContentProvider>
         <CustomCursor />
-        <Analytics /> {/* Vercel Analytics – must be inside <body> */}
       </body>
     </html>
   );
