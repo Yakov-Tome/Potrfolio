27b482488a8d0500a08d5ee60fc1ab22a71ac3a3
Yakov Tome <167262858+Yakov-Tome@users.noreply.github.com>
2025-04-06 11:41:18 +0300

    Add Skills & Update CV

--- app/pages/about/Skill.jsx
+++ app/pages/about/Skill.jsx
@@ -26,6 +26,7 @@ const skillsContent = [
   { skillName: "Photoshop", avatar: "/svg/ps.svg" },
   { skillName: "React Query", avatar: "/svg/query.png" },
   { skillName: "EJS", avatar: "/svg/ejs.png" },
+  { skillName: "Redux", avatar: "/svg/Redux.svg" },
 ];
 
 // Animation settings
